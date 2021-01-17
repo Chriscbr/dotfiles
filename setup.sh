b5f2e248f0ceec4ddc7ebe271de1719bbb860ebe
Chriscbr <crybicki98@gmail.com>
2021-01-17 06:42:33 -0500

    make setup.sh more POSIX compatible

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -eu
 
-# if [[ "$OSTYPE" == darwin* ]]; then
-#     # install macOS XCode command-line utilities
-#     xcode-select --install
+if [ "$OSTYPE" = darwin* ]; then
+    # install macOS XCode command-line utilities
+    xcode-select --install
 
-#     # install brew dependencies
-#     echo "Installing/updating brew dependencies..."
-#     brew bundle
-# fi
+    # install brew dependencies
+    echo "Installing/updating brew dependencies..."
+    brew bundle
+fi
 
 # install or update powerlevel10k, used for our custom zsh theme
-if [[ -d ~/powerlevel10k ]]; then
+if [ -d ~/powerlevel10k ]; then
     git -C ~/powerlevel10k pull
 else
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 fi
 
 # install or update oh-my-zsh, used for convenience plugins
-if [[ -d ~/.oh-my-zsh ]]; then
+if [ -d ~/.oh-my-zsh ]; then
     git -C ~/.oh-my-zsh pull
 else
     git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
@@ -26,7 +26,7 @@ fi
 
 # install zsh-auto-suggestions
 ZSH_AUTOSUGGESTIONS_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-if [[ -d $ZSH_AUTOSUGGESTIONS_PATH ]]; then
+if [ -d $ZSH_AUTOSUGGESTIONS_PATH ]; then
     git -C $ZSH_AUTOSUGGESTIONS_PATH pull
 else
     git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_AUTOSUGGESTIONS_PATH
@@ -34,7 +34,7 @@ fi
 
 # install zsh-syntax-highlighting
 ZSH_SYNTAX_HIGHLIGHTING_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-if [[ -d $ZSH_SYNTAX_HIGHLIGHTING_PATH ]]; then
+if [ -d $ZSH_SYNTAX_HIGHLIGHTING_PATH ]; then
     git -C $ZSH_SYNTAX_HIGHLIGHTING_PATH pull
 else
     git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_SYNTAX_HIGHLIGHTING_PATH
