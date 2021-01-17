17947a183a39292e12d432b76c1534fbf415beed
Chriscbr <crybicki98@gmail.com>
2021-01-17 07:03:07 -0500

    remove changing shell thing which hangs on github codespaces

--- setup.sh
+++ setup.sh
@@ -17,32 +17,27 @@ else
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 fi
 
-# # install or update oh-my-zsh, used for convenience plugins
-# if [ -d ~/.oh-my-zsh ]; then
-#     git -C ~/.oh-my-zsh pull
-# else
-#     git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-# fi
+# install or update oh-my-zsh, used for convenience plugins
+if [ -d ~/.oh-my-zsh ]; then
+    git -C ~/.oh-my-zsh pull
+else
+    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+fi
 
-# # install zsh-auto-suggestions
-# ZSH_AUTOSUGGESTIONS_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# if [ -d $ZSH_AUTOSUGGESTIONS_PATH ]; then
-#     git -C $ZSH_AUTOSUGGESTIONS_PATH pull
-# else
-#     git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_AUTOSUGGESTIONS_PATH
-# fi
+# install zsh-auto-suggestions
+ZSH_AUTOSUGGESTIONS_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ -d $ZSH_AUTOSUGGESTIONS_PATH ]; then
+    git -C $ZSH_AUTOSUGGESTIONS_PATH pull
+else
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_AUTOSUGGESTIONS_PATH
+fi
 
-# # install zsh-syntax-highlighting
-# ZSH_SYNTAX_HIGHLIGHTING_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# if [ -d $ZSH_SYNTAX_HIGHLIGHTING_PATH ]; then
-#     git -C $ZSH_SYNTAX_HIGHLIGHTING_PATH pull
-# else
-#     git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_SYNTAX_HIGHLIGHTING_PATH
-# fi
-
-echo $(which zsh)
-
-# try changing the shell to zsh
-chsh -s $(which zsh)
+# install zsh-syntax-highlighting
+ZSH_SYNTAX_HIGHLIGHTING_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ -d $ZSH_SYNTAX_HIGHLIGHTING_PATH ]; then
+    git -C $ZSH_SYNTAX_HIGHLIGHTING_PATH pull
+else
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_SYNTAX_HIGHLIGHTING_PATH
+fi
 
 echo "Setup complete!"
